6eb0dcd47f82cd3940ee359c6d4d4cd1b8529c76
AbedTakriti <85637491+AbedTakriti@users.noreply.github.com>
2021-12-13 22:03:50 +0200

    Update ArrayEx1.c

--- C_Programming/Assignment3/ArrayEx1.c
+++ C_Programming/Assignment3/ArrayEx1.c
@@ -1,55 +1,39 @@
-/*
- * Ex1.c
- *
- *  Created on: Jun 15, 2021
- *      Author: Abdulrahman
- */
 #include "stdio.h"
 
-int main()
+void main()
 
 {
-	float x[2][2];
-	float y[2][2];
-	int i,j;
-	printf("Enter the elements of the 1st Matrix\n");
+	int i, j;
+	float X1[2][2], X2[2][2];
+	printf("Enter first array elements\n");
+	fflush(stdin);
+	fflush(stdout);
+	for (i = 0; i < 2; i++) {
+		for (j = 0; j < 2; j++) {
 
-	for(i=0;i<2;i++)
-	{
-		for(j=0;j<2;j++)
-		{
+			scanf("%f", &X1[i][j]);
 
-			printf("Enter the a%d%d element:",i+1,j+1);
-			fflush(stdin);
-			fflush(stdout);
-			scanf("%f",&x[i][j]);
 		}
 	}
-	printf("\nEnter the elements of the 2nd Matrix\n");
-	for(i=0;i<2;i++)
-	{
-		for(j=0;j<2;j++)
-		{
-			printf("Enter the a%d%d element:",i+1,j+1);
-			fflush(stdin);
-			fflush(stdout);
-			scanf("%f",&y[i][j]);
+	printf("Enter second array elements\n");
+	fflush(stdin);
+	fflush(stdout);
+	for (i = 0; i < 2; i++) {
+		for (j = 0; j < 2; j++) {
+
+			scanf("%f", &X2[i][j]);
+
 		}
 	}
-	printf("\nThe Sum of Matrix:\n");
+	printf(" the sum of the two arrays: \n");
 
-	for(i=0;i<2;i++)
-	{
-		for(j=0;j<2;j++)
-		{
-
-			printf("%.2f ", x[i][j]+y[i][j]);
-
+	for (i = 0; i < 2; i++) {
+		for (j = 0; j < 2; j++) {
+			printf("%.2f", X1[i][j] + X2[i][j]);
+			printf(" ");
 		}
 		printf("\n");
 	}
-	return 0;
+
 }
-
-
 
