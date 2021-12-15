5ac75200d07ef9851dcc8a7015017cb2c26ea99b
AbedTakriti <85637491+AbedTakriti@users.noreply.github.com>
2021-12-15 23:55:15 +0200

    Update ArrayEx4.c

--- C_Programming/Assignment3/ArrayEx4.c
+++ C_Programming/Assignment3/ArrayEx4.c
@@ -1,44 +1,40 @@
-/*
- * Ex1.c
- *
- *  Created on: Jun 15, 2021
- *      Author: Abdulrahman
- */
 #include "stdio.h"
 
-int main()
+void main()
 
 {
-	int i,number,new_element,location;
-	printf("Enter the number of elements  :\n");
+	int i, num, position, place, size;
+	int X1[100];
+
+	printf("Please specify the size of array :\n");
 	fflush(stdin);
 	fflush(stdout);
-	scanf("%d",&number);
-	int x[number];
-	for(i=0;i<number;i++)
-		{
-			x[i]=i+1;
-			printf("%d ",x[i]);
-
+	scanf("%d", &size);
+	printf("Now enter the elements:\n");
+	fflush(stdin);
+	fflush(stdout);
+	for (i = 0; i < size; i++) {
+		scanf("%d", &X1[i]);
+	}
+	printf("Enter the number desired:\n");
+	fflush(stdin);
+	fflush(stdout);
+	scanf("%d", &num);
+	printf(" specify the position from 1 --> %d :\n", size);
+	fflush(stdin);
+	fflush(stdout);
+	scanf("%d", &position);
+	place = position - 1;
+	for (i = size - 1; i >= 0; i--) {
+		X1[i + 1] = X1[i];
+		if (i == place) {
+			X1[i] = num;
+			break;
 		}
+	}
+	printf("array after inserting:\n");
+	for (i = 0; i < size + 1; i++) {
+		printf("%d ", X1[i]);
+	}
 
-	printf("\nEnter element to be inserted: ");
-	fflush(stdin);
-	fflush(stdout);
-	scanf("%d",&new_element);
-	printf("Enter the location: ");
-	fflush(stdin);
-	fflush(stdout);
-	scanf("%d",&location);
-	for(i=0;i<number;i++)
-		{
-			if(i == location-1)
-			printf("%d ", new_element);
-			printf("%d ",x[i]);
-
-		}
-	return 0;
 }
-
-
-
